955e9915fdf8c82ffe3b11bc1ef4df54829bcbbc
brunopp <marduk.dk@gmail.com>
2017-07-05 21:07:37 +0200

    Improve picking the next word.  - Fix getting the same word in row.  - Change to getting all words marked as learning equally.

--- LearnerDictionary/Controllers/HomeController.cs
+++ LearnerDictionary/Controllers/HomeController.cs
@@ -43,7 +43,12 @@ namespace LearnerDictionary.Controllers
 
 			var relevantWords = new List<Word>();
 
-            var word = wordsInProcess.OrderBy(x => x.Score * rng.NextDouble() + rng.NextDouble()).FirstOrDefault();
+			var latestFiveWordIdAttempts = new HashSet<int>(_context.WordAttempts.OrderByDescending(x => x.CreatedUtc).Take(5).Select(x => x.WordId));
+
+			var take = wordsInProcess.Count() - latestFiveWordIdAttempts.Count;
+			var skip = rng.Next(take);
+
+			var word = wordsInProcess.Where(x => !latestFiveWordIdAttempts.Contains(x.Id)).Skip(skip).FirstOrDefault();
 
 			return View(new WordFormViewModel
 			{
